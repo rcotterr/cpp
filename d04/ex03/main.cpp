b98a1859b660918dc919a709fd4e253bb9f30b4a
marina.romashkova <Marinrom5256495$>
2021-11-04 23:57:27 +0300

    d04 ex03

--- d04/ex03/main.cpp
+++ d04/ex03/main.cpp
@@ -87,26 +87,25 @@ int main() {
     // connections (a lot of materia, no materia at all and so on)
 
 
-//    std::cout << "   ***check SubjectMain***" << std::endl;
-//
-//    IMateriaSource* src = new MateriaSource();
-//    src->learnMateria(new Ice());
-//    src->learnMateria(new Cure());
-//    ICharacter* me = new Character("me");
-//    AMateria* tmp;
-//    tmp = src->createMateria("ice");
-//    me->equip(tmp);
-//    tmp = src->createMateria("cure");
-//    me->equip(tmp);
-//    ICharacter* bob = new Character("bob");
-//    me->use(0, *bob);
-//    me->use(1, *bob);
-//    delete bob;
-//    delete me;
-//    delete src;
-//
-//    std::cout << std::endl;
-//    std::cout << "   ***end of check SubjectMain***" << std::endl;
+    std::cout << "   ***check SubjectMain***" << std::endl;
+
+    IMateriaSource* src = new MateriaSource();
+    src->learnMateria(new Ice());
+    src->learnMateria(new Cure());
+    ICharacter* me = new Character("me");
+    AMateria* tmp;
+    tmp = src->createMateria("ice");
+    me->equip(tmp);
+    tmp = src->createMateria("cure");
+    me->equip(tmp);
+    ICharacter* bob = new Character("bob");
+    me->use(0, *bob);
+    me->use(1, *bob);
+    delete bob;
+    delete me;
+    delete src;
+
+    std::cout << "   ***end of check SubjectMain***" << std::endl;
 
 
     //ice and cure deleted while destructor for character2
