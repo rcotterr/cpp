61c416e80e7b089b9bf38d4f26b4cd9407aab58e
marina.romashkova <Marinrom5256495$>
2021-11-09 13:26:10 +0300

    d05 ex02

--- d05/ex02/ShrubberyCreationForm.cpp
+++ d05/ex02/ShrubberyCreationForm.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <iostream>
 #include <string>
 #include "ShrubberyCreationForm.hpp"
@@ -5,6 +6,17 @@
 const int ShrubberyCreation__exec_grade = 137;
 const int ShrubberyCreation__sign_grade = 145;
 const std::string ShrubberyCreation__name = "ShrubberyCreationForm";
+const std::string trees =
+"               ,@@@@@@@,\n"
+"       ,,,.   ,@@@@@@/@@,  .oo8888o.\n"
+"    ,&%%&%&&%,@@@@@/@@@@@@,8888\\88/8o\n"
+"   ,%&\\%&&%&&%,@@@\\@@@/@@@88\\88888/88'\n"
+"   %&&%&%&/%&&%@@\\@@/ /@@@88888\\88888'\n"
+"   %&&%/ %&%%&&@@\\ V /@@' '88\\8 `/88'\n"
+"   `&%\\ ` /%&'    |.|         | |8'\n"
+"       |o|        | |         | |\n"
+"       |.|        | |         | |\n"
+"    \\/ ._\\//_/__/  ,\\_//__\\/.  \\_//__/_\n";
 
 ShrubberyCreationForm::ShrubberyCreationForm(std::string target) : Form(ShrubberyCreation__name, ShrubberyCreation__sign_grade, ShrubberyCreation__exec_grade) {
     this->_target = target;
@@ -33,8 +45,9 @@ std::string ShrubberyCreationForm::getTarget() const {
 void ShrubberyCreationForm::execute(Bureaucrat const & executor) const {
 
     if (Form::validateExecution(executor)) {
-        std::cout << "some execution here "<< std::endl; //TODO execute
+        std::ofstream out(this->_target + "_shrubbery");
+        out << trees;
+        out.close();
     }
-
     return;
 }
