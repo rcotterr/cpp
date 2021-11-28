a49f5f778475ccf4a7925f5ab688243c0bd89964
marina.romashkova <Marinrom5256495$>
2021-11-28 15:03:15 +0300

    d06 ex03

--- d06/ex00/main.cpp
+++ d06/ex00/main.cpp
@@ -3,6 +3,10 @@
 #include <typeinfo>
 #include <sstream>
 #include <errno.h>
+#include <regex>
+//#include <QString>
+#include <cstdlib>
+#include <stdlib.h>
 
 
 bool is_displayable_char(std::string arg) {
@@ -17,12 +21,68 @@ void process_char(char c) {
     std::cout << "int: " << i << std::endl;
 
     float f = static_cast<float>(c);
-    std::cout << "float: " << f << std::endl;
+    std::cout << "float: " << f << ".0f" << std::endl;
 
     double d = static_cast<double>(c);
-    std::cout << "double: " << d << std::endl;
-
+    std::cout << "double: " << d << ".0" << std::endl;
 }
+
+
+bool is_int(char * arg) {
+    char *end;
+    long  l;
+    errno = 0;
+    l = std::strtol(arg, &end, 10);
+    if ((errno == ERANGE && l == LONG_MAX) || l > INT_MAX) {
+        return false;
+    }
+    if ((errno == ERANGE && l == LONG_MIN) || l < INT_MIN) {
+        return false;
+    }
+    if (*arg == '\0' || *end != '\0') {
+        return false;
+    }
+    return true;
+}
+
+
+void process_int(int n) {
+
+    char c = static_cast<char>(n);
+
+    if ((bool)std::isprint(c)) {
+        std::cout << "char: " << c << std::endl;
+    }
+    else {
+        std::cout << "char: Non displayable" << std::endl;
+    }
+
+    std::cout << "int: " << n << std::endl;
+
+    float f = static_cast<float>(n);
+    std::cout << "float: " << f << ".0f" << std::endl;
+
+    double d = static_cast<double>(n);
+    std::cout << "double: " << d << ".0" << std::endl;
+}
+
+
+//bool is_int(char * arg) {
+//    char *end;
+//    long  l;
+//    errno = 0;
+//    l = std::strtol(arg, &end, 10);
+//    if ((errno == ERANGE && l == LONG_MAX) || l > INT_MAX) {
+//        return false;
+//    }
+//    if ((errno == ERANGE && l == LONG_MIN) || l < INT_MIN) {
+//        return false;
+//    }
+//    if (*arg == '\0' || *end != '\0') {
+//        return false;
+//    }
+//    return true;
+//}
 
 
 int main(int argc, char **argv) {
@@ -38,38 +98,77 @@ int main(int argc, char **argv) {
         return 0;
     }
 
-    char *endptr = NULL;
-    if (arg[strlen(arg) - 1] == 'f') { //TODO 123f
-        arg[strlen(arg) - 1] = '\0';
-    }
-
-    long double ld = std::strtold(arg, &endptr);
-    std::cout << "ld: " << ld << std::endl; //delete
-
-    if (errno != 0 || arg == endptr || *endptr != 0) { //TODO more errors https://stackoverflow.com/questions/26080829/detecting-strtol-failure/26083517
-        std::cout << "char: impossible" << std::endl;
-        std::cout << "int: impossible" << std::endl;
-        std::cout << "float: impossible" << std::endl;
-        std::cout << "double: impossible" << std::endl;
+    if (is_int(arg)) {
+        std::cout << "is int" << std::endl;
+        int n = std::stoi(arg);
+        process_int(n);
         return 0;
     }
 
-    char c = static_cast<char>(ld);
-    if ((bool)std::isprint(c)) {
-        std::cout << "char: " << c << std::endl;
+    if (is_float(arg)) {
+        std::cout << "is float" << std::endl;
+//        int n = std::stoi(arg);
+//        process_int(n);
+        return 0;
     }
-    else {
-        std::cout << "char: Non displayable" << std::endl;
-    }
 
-    int i = static_cast<int>(ld);
-    std::cout << "int: " << i << std::endl;
+//    int i_;
+//    std::cout << sscanf("1231q", "%d", &i_) << std::endl;
+//    std::cout << sscanf("1231", "%d", &i_) << std::endl;
+//    std::cout << sscanf("1231q.0", "%d", &i_) << std::endl;
+//    std::cout << sscanf("-12", "%d", &i_) << std::endl;
+//    std::cout << sscanf("1231.0f", "%d", &i_) << std::endl;
+//
+//    if(sscanf("1231q", "%d", &i_) != 1) {
+//        std::cout << "int" << std::endl;
+//        return 0;
+//    }
 
-    float f = static_cast<float>(ld);
-    std::cout << "float: " << f << std::endl;
+//    bool ok;
+//    std::string str = "123";
+//
+//    int i = QString::fromStdString(str).toInt(&ok);
+//    if (ok) {
+//        std::cout << "int" << std::endl;
+//        return;
+//    }
 
-    double d = static_cast<double>(ld);
-    std::cout << "double: " << d << std::endl;
+
+//    char *endptr = NULL;
+////    if (arg[strlen(arg) - 1] == 'f') { //TODO 123f
+////        arg[strlen(arg) - 1] = '\0';
+////    }
+//
+//    long double ld = std::strtold(arg, &endptr);
+//    std::cout << "ld: " << ld << std::endl; //delete
+//
+//    std::cout << "endptr " << *endptr << std::endl;
+//
+////    if (errno != 0 || arg == endptr || (*endptr != 0 || ())) { //TODO more errors https://stackoverflow.com/questions/26080829/detecting-strtol-failure/26083517
+//    if (errno != 0 || arg == endptr || *endptr != 0) { //TODO more errors https://stackoverflow.com/questions/26080829/detecting-strtol-failure/26083517
+//        std::cout << "char: impossible" << std::endl;
+//        std::cout << "int: impossible" << std::endl;
+//        std::cout << "float: impossible" << std::endl;
+//        std::cout << "double: impossible" << std::endl;
+//        return 0;
+//    }
+//
+//    char c = static_cast<char>(ld);
+//    if ((bool)std::isprint(c)) {
+//        std::cout << "char: " << c << std::endl;
+//    }
+//    else {
+//        std::cout << "char: Non displayable" << std::endl;
+//    }
+//
+//    int i = static_cast<int>(ld);
+//    std::cout << "int: " << i << std::endl;
+//
+//    float f = static_cast<float>(ld);
+//    std::cout << "float: " << f << std::endl;
+//
+//    double d = static_cast<double>(ld);
+//    std::cout << "double: " << d << std::endl;
 
     return 0;
 }
@@ -106,3 +205,8 @@ int main(int argc, char **argv) {
 //0.0f, -4.2f, 4.2f
 //-inff, +inff and nanf.
 //0.0, -4.2, 4.2
+//"1234567.0f   "
+
+
+
+//https://stackoverflow.com/questions/7663709/how-can-i-convert-a-stdstring-to-int
