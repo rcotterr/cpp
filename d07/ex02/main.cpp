270bc53e30de2e0c8e24b8b5ebb87ba7b4ccb2ce
marina.romashkova <Marinrom5256495$>
2021-12-01 23:40:01 +0300

    d07 ex02

--- d07/ex02/main.cpp
+++ d07/ex02/main.cpp
@@ -12,25 +12,5 @@ int main(void) {
     std::cout << "Array size of b is: " << b.size() << std::endl;
 
 
-
-//    const int len_arr_int = 3;
-//    const int len_arr_char = 5;
-//    const int len_arr_float = 2;
-//    int arr_int[len_arr_int] = {1, 2, 3};
-//    char arr_char[len_arr_char] = {'a', 'b', 'c', 'd', 'e'};
-//    float arr_float[len_arr_float] = {1.5, 2.0};
-//
-//    std::cout << "***check int array with foo***" << std::endl;
-//    iter<int>(arr_int, len_arr_int, foo);
-//    std::cout << std::endl;
-//
-//    std::cout << "***check char array with another_foo***" << std::endl;
-//    iter<char>(arr_char, len_arr_char, another_foo);
-//    std::cout << std::endl;
-//
-//    std::cout << "***check float array with one_more_foo***" << std::endl;
-//    iter<float>(arr_float, len_arr_float, one_more_foo);
-//    std::cout << std::endl;
-
     return 0;
 }
