0bd357f79664ad6c15728bb4b0bcae1ca07d611a
marina.romashkova <Marinrom5256495$>
2021-12-01 21:40:36 +0300

    d07 ex02

---
+++ d07/ex02/Array.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+//
+//template <typename T>
+//void   iter(T * const arr_address, int len, void (*foo)(T const &)) {
+//    for (int i = 0; i < len; i++) {
+//	    T var = arr_address[i];
+//        foo(var);
+//    }
+//    return;
+//}
+
+
