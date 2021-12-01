2a4694430d49181edf53667f6a7c89725dcc555e
marina.romashkova <Marinrom5256495$>
2021-12-01 23:39:28 +0300

    d07 ex02

--- d07/ex02/Array.hpp
+++ d07/ex02/Array.hpp
@@ -1,14 +1,46 @@
 #include <iostream>
 #include <string>
 
-//
-//template <typename T>
-//void   iter(T * const arr_address, int len, void (*foo)(T const &)) {
-//    for (int i = 0; i < len; i++) {
-//	    T var = arr_address[i];
-//        foo(var);
-//    }
-//    return;
-//}
 
+
+template <typename T>
+class Array {
+    public:
+        Array<T>() {
+            std::cout << "constructor0" << std::endl;
+            this->_len = 0;
+            this->_arr = new int[this->_len];
+            return;
+        };
+
+        Array<T>(unsigned int n) {
+            std::cout << "constructor1" << std::endl;
+            this->_len = n;
+            this->_arr = new int[this->_len];
+            for (unsigned int i = 0; i < this->_len; i++) {
+                this->_arr[i] = T();
+//                std::cout << this->_arr[i] << std::endl;
+            }
+            return;
+        };
+
+
+        ~Array<T>(){
+            return;
+        };
+        unsigned int size(void) const {
+            return this->_len;
+        }
+
+    //TODO Construction by copy and assignment operator. In both cases, modifying one of
+    //the two arrays after copy/assignment won’t affect anything in the other array
+
+    //TODO Elements are accessible through the operator[].
+    //TODO • When accessing an element with the operator[], if this element is out of the limits,
+    //a std::exception is thrown
+    private:
+        T * _arr;
+        unsigned int _len;
+
+};
 
--- d07/ex02/main.cpp
+++ d07/ex02/main.cpp
@@ -5,6 +5,11 @@
 
 int main(void) {
 
+    std::cout << "***check Array***" << std::endl;
+    Array<int> a;
+    Array<int> b(4);
+
+    std::cout << "Array size of b is: " << b.size() << std::endl;
 
 
 
