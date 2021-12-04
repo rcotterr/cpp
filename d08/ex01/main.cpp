575fd357ef434143e39573da1451370d707cbb71
marina.romashkova <Marinrom5256495$>
2021-12-04 16:25:02 +0300

    d08 ex01

--- d08/ex01/main.cpp
+++ d08/ex01/main.cpp
@@ -6,7 +6,7 @@
 
 int main(void) {
     Span sp = Span(5);
-    std::cout << sp << std::endl;
+    std::cout << sp;
     sp.addNumber(4);
     sp.addNumber(3);
     sp.addNumber(2);
@@ -14,6 +14,10 @@ int main(void) {
     sp.addNumber(0);
     std::cout << sp << std::endl;
 
+    Span sp_copy = Span(sp);
+    std::cout << "After copy constructor sp_copy is: " << sp_copy;
+    Span sp_equal = sp;
+    std::cout << "After operator= sp_equal is: " << sp_equal;
 
     return 0;
 }
