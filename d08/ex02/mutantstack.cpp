f3f73dee4f3d9a2cfddaf38c6a14c1dd6bb3a75d
marina.romashkova <Marinrom5256495$>
2021-12-11 13:06:44 +0300

    d08 ex02

--- d08/ex02/mutantstack.cpp
+++ d08/ex02/mutantstack.cpp
@@ -1,39 +1,35 @@
 #include <iostream>
 #include "mutantstack.hpp"
 
+
 template<typename T, typename Container>
-MutantStack<T, Container>::MutantStack( const MutantStack<T,Container>  & src) {*this = src; return ;}
+MutantStack<T, Container>::MutantStack(MutantStack<T,Container> const & src) {
+    *this = src;
+    return;
+}
 
+template<typename T, typename Container>
+MutantStack<T, Container>::MutantStack() {
+    return;
+}
 
-//template<typename T>
-//MutantStack<T>::MutantStack() {
-//    return;
-//}
+template<typename T, typename Container>
+MutantStack<T, Container>::~MutantStack() {
+    return;
+}
 
-//template<typename T, typename Container>
-//MutantStack<T, Container>::MutantStack() {return;}
+template<typename T, typename Container>
+MutantStack<T, Container> & MutantStack<T, Container>::operator=(MutantStack const & src) {
+    this->c = src.c;
+    return *this;
+}
 
+template<typename T, typename Container>
+typename MutantStack<T, Container>::iterator MutantStack<T, Container>::begin() {
+    return this->c.begin();
+}
 
-//template<typename T, typename Container>
-//MutantStack<T, Container>::MutantStack( const MutantStack<T,Container>  & src) {(void)src;}
-//MutantStack<T, Container>::MutantStack(MutantStack const & src) {(void)src;}
-
-
-//MutantStack::~MutantStack() {
-//    return;
-//}
-//
-//MutantStack & MutantStack::operator=(MutantStack const & src) {
-//    (void)src;
-//    return *this;
-//}
-//
-//MutantStack::MutantStack(MutantStack const & src) {
-//    *this = src;
-//    return;
-//}
-//
-//void MutantStack::iterator() {
-//    //TODO iterator
-//    return;
-//}
+template<typename T, typename Container>
+typename MutantStack<T, Container>::iterator MutantStack<T, Container>::end() {
+    return this->c.end();
+}
--- d08/ex02/mutantstack.hpp
+++ d08/ex02/mutantstack.hpp
@@ -9,27 +9,14 @@ template <class T, typename Container = std::deque<T> >
 class MutantStack : public std::stack<T>
 {
     public:
-        MutantStack() {};
-        virtual ~MutantStack() {return;}
-        MutantStack & operator=(MutantStack const & src) { this->c = src.c; return *this;}
+        MutantStack();
+        ~MutantStack();
+        MutantStack & operator=(MutantStack const & src);
         MutantStack( const MutantStack<T, Container> & src);
-//        MutantStack(MutantStack const & src);
 
         typedef typename std::stack<T>::container_type::iterator iterator;
-        iterator begin() { return this->c.begin(); }
-        iterator end() { return this->c.end(); }
-
+        iterator begin();
+        iterator end();
 };
-
-//template<typename T, typename Container>
-//MutantStack<T, Container>::MutantStack() {return;}
-
-
-
-
-//template<typename T, typename Container>
-//void MutantStack<T, Container>::iterator(void) {
-//    return;
-//}
 
 #endif
